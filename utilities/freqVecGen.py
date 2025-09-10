338740502782f8864ab6b36b028ff32edc4b658b
dyy5079 <dyy5079@psu.edu>
2025-09-10 19:09:50 -0400

    Initial Creation
    freqVecGen.py added to utilities dir

---
+++ utilities/freqVecGen.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def freq_vec_gen(Nt, fs):
+    """
+    Generates a vector of frequencies that correspond to the elements of an FFT
+    with Nt samples at fs sampling rate. Works for both odd and even length signals.
+    The frequencies are arranged in order of positive frequencies (0 to fs/2)
+    followed by the negative frequencies (-fs/2 to 0-df).
+
+    Parameters:
+    Nt (int): Number of samples
+    fs (float): Sampling frequency in Hz
+
+    Returns:
+    np.ndarray: Vector of frequencies
+    """
+    df = fs / Nt  # frequency step
+
+    if Nt % 2 == 0:  # even number of points
+        freq_vec = np.arange(-Nt//2, Nt//2) * df
+    else:  # odd number of points
+        Nt_adj = Nt - 1
+        freq_vec = np.arange(-Nt_adj//2, Nt_adj//2 + 1) * df
+
+    pos_freq_mask = freq_vec >= 0
+    freq_vec = np.concatenate((freq_vec[pos_freq_mask], freq_vec[~pos_freq_mask]))
+
+    return freq_vec
